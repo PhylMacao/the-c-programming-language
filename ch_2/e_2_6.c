48b55a0511dd261af52661050a5bc141b4abecbd
PhylMacao <13609252+PhylMacao@users.noreply.github.com>
2022-04-24 07:06:21 +0200

    Solution for 2-6.

---
+++ ch_2/e_2_6.c
@@ -0,0 +1,28 @@
+/* 
+ * Write a function setbits(x,p,n,y) that returns x with the n bits that begin
+ * at position p set to the rightmost n bits of y, leaving the other bits
+ * unchanged.
+*/
+#include <stdio.h>
+
+unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y);
+
+int main()
+{
+    printf("%u\n", setbits(255, 1, 7, 0));
+    return 0;
+}
+
+unsigned setbits(unsigned x, unsigned p, unsigned n, unsigned y)
+{
+    /*
+     * counting from the right, we need to do
+     *    ...p...0
+     * xxxx000xxxx
+     * 0000yyy0000
+     * xxxxyyyxxxx
+    */
+    x = x & ~(~(~0u << n) << (p));
+    y = y & (~(~0u << n) << (p));
+    return x | y;
+}
